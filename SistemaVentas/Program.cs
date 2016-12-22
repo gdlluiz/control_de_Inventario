639c5d90bcdbe53465a6867131ee255f25f2006b
Luiz <gdlluiz@gmail.com>
2016-12-22 11:15:02 -0600

    Add files via upload
    Se agrega capa principal, maneja todos los forularios

---
+++ SistemaVentas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Importo CapaPresentacio para que se comunique con la misma
+using CapaPresentacion;
+
+namespace SistemaVentas
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // indico que  corra el formularioFrmCategoria
+            Application.Run(new frmLogin());
+        }
+    }
+}
